64ef0df6f6e7d5cd635138f645666a9c1d80fa59
yamacho <yamacho@users.noreply.github.com>
2020-03-04 22:24:37 +0900

    アクセス時刻の表示を見やすく変更

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     const now = Date.now();
     res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-    res.end(req.headers.cookie);
+
+    const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+    res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
